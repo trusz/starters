b12bb5014d3205dc58ee7a0893bbb0265cf2b865
Tamás Rusz <t.rusz@sprinteins.com>
2020-03-13 08:50:08 +0100

    refactor(pul): remove unfinished starter kit

--- web-front-end/product-ui-library/ui-lib-01/src/components/button/button.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-
-export function Button(props: Props) {
-    return <button onClick={props.onClick} >Hi I am a button</button>
-}
-
-interface Props {
-    onClick(): void
-}
